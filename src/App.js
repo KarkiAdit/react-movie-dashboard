b3f9a43e009689b97ae89e6385741231fdab52ab
Aditya Karki <adityakarki728@gmail.com>
2022-09-16 21:59:04 -0500

    Imported bootstrap designs and added useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import MovieList from "./components/MovieList"
+import { useEffect } from "react";
+
 
 function App() {
-  return <div className="App"></div>;
+  const [movies, setMovies] = useState([])
+  const [searchValue, setSearchValue]
+
+const getMovieRequest = async () =>{
+  const url = "http://www.omdbapi.com/?s=avengers&apikey=85f29b73"
+  const response = await fetch(url);  
+  const responseJSON = await response.json()
+  console.log(responseJSON)
+  setMovies(responseJSON.Search)
+}
+
+useEffect(()=> {
+  getMovieRequest();
+}, [])
+
+  return (
+    <div className="container-fluid movie-app">
+      <div className="row"><MovieList movies={movies}/></div>
+    </div>
+  );
 }
 
 export default App;
