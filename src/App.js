b6f8f138b906535d96003eeb130fb6ea8dc8e8fd
Aditya Karki <adityakarki728@gmail.com>
2022-10-03 23:57:56 -0500

    Added the remove and local storage function code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,71 @@
 import React, { useState, useEffect } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import MovieList from "./components/MovieList"
-import MovieListHeading from "./components/MovieListHeading"
-import SearchBox from "./components/SearchBox"
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 import AddFavourites from "./components/AddFavourites";
+import RemoveFavourites from "./components/RemoveFavourites";
 
 function App() {
-  const [movies, setMovies] = useState([])
-  const [searchValue, setSearchValue] = useState("")
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [favourites, setFavourites] = useState([]);
 
-const getMovieRequest = async (searchValue) =>{
-  const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=85f29b73`
-  const response = await fetch(url);  
-  const responseJSON = await response.json()
-  if (responseJSON.Search){
-    setMovies(responseJSON.Search)
-  }
-}
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=85f29b73`;
+    const response = await fetch(url);
+    const responseJSON = await response.json();
+    if (responseJSON.Search) {
+      setMovies(responseJSON.Search);
+    }
+  };
 
-useEffect(()=> {
-  getMovieRequest(searchValue);
-}, [searchValue])
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  const addToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-storage", JSON.stringify(items));
+  };
+
+  const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie];
+    setFavourites(newFavouriteList);
+    addToLocalStorage(newFavouriteList);
+  };
+
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+    setFavourites(newFavouriteList);
+    addToLocalStorage(newFavouriteList);
+  };
 
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
-        <MovieListHeading heading="Movies"/>
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
-        <MovieList movies={movies} favComponent={AddFavourites}/></div>
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={addFavouriteMovie}
+          favComponent={AddFavourites}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourites" />
+      </div>
+      <div className="row">
+        <MovieList
+          handleFavouritesClick={removeFavouriteMovie}
+          movies={favourites}
+          favouriteComponent={RemoveFavourites}
+        />
+      </div>
     </div>
   );
 }
