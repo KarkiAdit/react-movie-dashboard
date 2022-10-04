21a04f1d96c698eaebeabdad04c43f4a18f80832
Aditya Karki <adityakarki728@gmail.com>
2022-10-03 20:42:49 -0500

    Added the add favourites feature

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 const MovieList = (props) => {
+  const Favourite = props.favComponent;
   return (
     <>
       {props.movies.map((movie, idx) => (
         <div className="image-container d-flex justify-content-start m-3">
           <img src={movie.Poster} alt="movie"></img>
-          <div className="overlay d-flex align-items-center justify-content-center"></div>
+          <div className="overlay d-flex align-items-center justify-content-center">
+            <Favourite />
+          </div>
         </div>
       ))}
     </>
