b7d867a6bfc5fb95b87203d05b028c9c61966ae9
Aditya Karki <adityakarki728@gmail.com>
2022-10-04 00:00:16 -0500

    Edited the unnecessary code

--- src/components/MovieListHeading.js
+++ src/components/MovieListHeading.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const MovieListHeading = (props) => {
-  const favComponent = props.favComponent;
   return (
     <div className="col">
       <h1>{props.heading}</h1>
